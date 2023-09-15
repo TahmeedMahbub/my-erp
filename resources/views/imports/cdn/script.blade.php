ed976b96087fff84881ccb76d0ecfb0c3d37ac91
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2023-09-15 22:31:44 +0600

    select2 installed

--- resources/views/imports/cdn/script.blade.php
+++ resources/views/imports/cdn/script.blade.php
@@ -21,3 +21,4 @@
 <script src="{{asset('assets/plugins/imask/imask.js')}}"></script>
 <script src="{{asset('assets/pages/forms-advanced.js')}}"></script>
 <script src="{{ asset('assets/pages/form-validation.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js" integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
--- resources/views/imports/cdn/style.blade.php
+++ resources/views/imports/cdn/style.blade.php
@@ -7,4 +7,5 @@
 <link href="{{asset('assets/plugins/select/selectr.min.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('assets/plugins/huebee/huebee.min.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('assets/plugins/datepicker/datepicker.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
