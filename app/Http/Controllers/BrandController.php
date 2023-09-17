e3028faf501e7acd937c142c39ee2ad12e726258
Tahmeed Mahbub <ontikdev.3@gmail.com>
2023-09-17 10:57:12 +0600

    brand full remain

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\History;
-use App\Models\Role;
+use App\Models\Item;
+use App\Models\Unit;
 use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
@@ -14,114 +15,123 @@ class BrandController extends Controller
 {
     public function index()
     {
-        $roles = Role::all();
-        return view('role.index', compact('roles'));
+        $units = Unit::all();
+        return view('unit.index', compact('units'));
     }
 
     public function create()
     {
-        $roles = Role::all();
-        return view('role.create', compact('roles'));
+        $units = Unit::all();
+        return view('unit.create', compact('units'));
     }
 
     public function store(Request $request)
     {
-        $role = new Role;
-        $role->role_name = $request->role_name;
-        $role->details = $request->role_details;
-        $role->manager_role = $request->manager_role;
-        $role->created_by = Auth::user()->id;
-        $role->created_at = Carbon::now()->toDateTimeString();
-        $role->updated_by = Auth::user()->id;
-        $role->updated_at = Carbon::now()->toDateTimeString();
-        $role->save();
+        $unit = new Unit;
+        $unit->name = $request->name;
+        $unit->details = $request->details;
+        $unit->base_unit = $request->base_unit;
+        $unit->created_by = Auth::user()->id;
+        $unit->created_at = Carbon::now()->toDateTimeString();
+        $unit->updated_by = Auth::user()->id;
+        $unit->updated_at = Carbon::now()->toDateTimeString();
+        $unit->save();
 
         $history = new History;
-        $history->module = "Role";
-        $history->module_id = $role->id;
+        $history->module = "Unit";
+        $history->module_id = $unit->id;
         $history->operation = "Create";
         $history->previous = null;
-        $history->after = json_encode($role);
+        $history->after = json_encode($unit);
         $history->user_id = Auth::user()->id;
         $history->ip_address = Session::get('user_ip');
         $history->save();
 
         return redirect()
-        ->route('roles')
+        ->route('unit')
         ->with('alert.status', 'success')
-        ->with('alert.message', 'Role Created Successfully!');
+        ->with('alert.message', 'Unit Created Successfully!');
     }
 
     public function edit($id)
     {
-        $role = Role::find($id);
-        $roles = Role::all();
-        return view('role.edit', compact('role', 'roles'));
+        $unit = Unit::find($id);
+        $units = Unit::all();
+        return view('unit.edit', compact('unit', 'units'));
     }
 
     public function update(Request $request)
     {
-        $role = Role::find($request->id);
-        $old_role = clone $role;
+        $unit = Unit::find($request->id);
+        $old_role = clone $unit;
 
-        $role->role_name = $request->role_name;
-        $role->manager_role = $request->manager_role;
-        $role->details = $request->role_details;
-        $role->updated_by = Auth::user()->id;
-        $role->updated_at = Carbon::now()->toDateTimeString();
-        $role->save();
+        $unit->name = $request->name;
+        $unit->base_unit = $request->base_unit;
+        $unit->details = $request->details;
+        $unit->updated_by = Auth::user()->id;
+        $unit->updated_at = Carbon::now()->toDateTimeString();
+        $unit->save();
 
         $history = new History;
-        $history->module = "Role";
-        $history->module_id = $role->id;
+        $history->module = "Unit";
+        $history->module_id = $unit->id;
         $history->operation = "Edit";
         $history->previous = json_encode($old_role);
-        $history->after = json_encode($role);
+        $history->after = json_encode($unit);
         $history->user_id = Auth::user()->id;
         $history->ip_address = Session::get('user_ip');
         $history->save();
 
         return redirect()
-        ->route('roles')
+        ->route('unit')
         ->with('alert.status', 'success')
-        ->with('alert.message', 'Role Edited Successfully!');
+        ->with('alert.message', 'Unit Edited Successfully!');
     }
 
     public function delete($id)
     {
-        $role = Role::where('id', $id)->first();
+        $item = Item::where('unit_id', $id)->first();
+        if(!empty($item))
+        {
+            return redirect()
+            ->route('unit')
+            ->with('alert.status', 'danger')
+            ->with('alert.message', 'Few Items Are Using This Unit. It Cannot Be Deleted!!!');
+        }
 
-        if($role)
+        $unit = Unit::where('id', $id)->first();
+
+        if($unit)
         {
             $history = new History;
-            $history->module = "Role";
-            $history->module_id = $role->id;
+            $history->module = "Unit";
+            $history->module_id = $unit->id;
             $history->operation = "Delete";
-            $history->previous = json_encode($role);
+            $history->previous = json_encode($unit);
             $history->after = null;
             $history->user_id = Auth::user()->id;
             $history->ip_address = Session::get('user_ip');
             $history->save();
 
-            $role->delete();
+            $unit->delete();
 
             return redirect()
-            ->route('roles')
+            ->route('unit')
             ->with('alert.status', 'success')
-            ->with('alert.message', 'Role Deleted Successfully!');
+            ->with('alert.message', 'Unit Deleted Successfully!');
         }
         else
         {
             return redirect()
-            ->route('roles')
+            ->route('unit')
             ->with('alert.status', 'danger')
-            ->with('alert.message', 'This Role Cannot be Deleted!');
+            ->with('alert.message', 'This Unit Cannot be Deleted!');
         }
     }
 
-    public function userByRole($role_id)
+    public function userByUnit($unit_id)
     {
-        $users = User::where('role_id', $role_id)->where('id', '>', 0)->get();
-        return response()->json(['role_id' => $role_id, 'users' => $users]);
+        $users = User::where('role_id', $unit_id)->where('id', '>', 0)->get();
+        return response()->json(['role_id' => $unit_id, 'users' => $users]);
     }
 }
